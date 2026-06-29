83f3e611aaec54060402c414a81fc414262e0bb9
gipicihuy <154876480+gipicihuy@users.noreply.github.com>
2026-06-29 15:21:03 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@napi-rs/canvas', 'fake-ff')
+    }
+    return config
+  },
+}
 module.exports = nextConfig
